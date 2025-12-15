4b15ed5ea132a542fc02d82eaeb790190a7c6df3
Yohanan <yohananconsult@gmail.com>
2025-12-15 08:45:06 +0000

    Changed Build Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,27 @@
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === "production";
+// const basePath = isProd ? `/venus-nextjs` : "";
+
+// const nextConfig = {
+//   output: "export",
+//   basePath: "/brp-engineering-lab",
+//   assetPrefix: basePath,
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH: basePath,
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? `/venus-nextjs` : "";
-
 const nextConfig = {
   output: "export",
-  basePath: "/brp-engineering-lab",
-  assetPrefix: basePath,
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
